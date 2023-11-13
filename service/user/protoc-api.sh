fbe043fa696fdad44051730234140054960c220a
whs5280 <13434118350@163.com>
2023-11-13 11:26:23 +0800

    feat: window环境解决 `make api` 指定问题

---
+++ service/user/protoc-api.sh
@@ -0,0 +1,9 @@
+
+# make api 指令会自动执行此脚本
+protoc --proto_path=./api \
+       --proto_path=./third_party \
+               --go_out=paths=source_relative:./api \
+               --go-http_out=paths=source_relative:./api \
+               --go-grpc_out=paths=source_relative:./api \
+       --openapi_out=fq_schema_naming=true,default_response=false:. \
+       api/user/v1/user.proto
