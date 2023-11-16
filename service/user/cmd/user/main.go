ca6c6bd5bbacc5ce077d8a4568a44efd4bf40ec0
whs5280 <13434118350@163.com>
2023-11-16 11:39:24 +0800

    feat: 处理微服务启动遇到的问题

--- service/user/cmd/user/main.go
+++ service/user/cmd/user/main.go
@@ -19,9 +19,9 @@ import (
 // go build -ldflags "-X main.Version=x.y.z"
 var (
 	// Name is the name of the compiled software.
-	Name string
+	Name = "shop.user.service"
 	// Version is the version of the compiled software.
-	Version string
+	Version = "v1"
 	// flagconf is the config flag.
 	flagconf string
 
